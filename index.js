254d0a2af06c62a5ce5c636079201d9dc4a227cd
matthewxvoorhees <matthew.crnich@gmail.com>
2020-11-18 12:08:26 -0700

    commit tree 7fd93f27f8a2b1f1ea96ceb8eacdd887c43116ba

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, word){
+    let array = []
+    for (let i=0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${word} gift!`)
+    }
+    return array
+}
+
+function countDown(x){
+    if (x > 0) {
+        while (x >= 0) {
+            console.log(x)
+            x--
+        }
+    }
+}
